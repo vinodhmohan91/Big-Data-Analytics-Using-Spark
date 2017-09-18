a1cea38a850fd4f9c093ad8426cd5f1caa038f96
Vinodh Mohan <vinodhmohan91@gmail.com>
2017-09-18 19:24:57 -0400

    Added RE algorithm

--- RecommendationEngine.py
+++ RecommendationEngine.py
@@ -1,2 +1,26 @@
 #Recommendation Engine
 
+#Load the data
+ratingsData = SpContext.textFile("./Data/UserItemData.txt")
+ratingsData.collect()
+
+#Convert the string into vector
+ratingsVector = ratingsData.map(lambda l: l.split(',')) \
+                           .map(lambda l: (int(l[0]), int(l[1]), float(l[2])))
+
+ratingsDF = SpSession.createDataFrame(ratingsVector,['user','item','rating'])
+
+#Building the model based on ALS
+from pyspark.ml.recommendation import ALS
+als = ALS(rank=10, maxIter=5)
+model = als.fit(ratingsDF)
+
+model.userFactors.orderBy("id").collect()
+
+#Create a test data set of items you want ratings for
+testDF = SpSession.createDataFrame([(1001,9003),(1001,9004),(1001,9005)], \
+                                    ['user','item'])
+
+#Predict
+predictions = model.transform(testDF)
+predictions.collect()
